ed6978f0b1b8de839a0ec32d404c1bad30f8e4b5
mitsch01 <schwartz.miriam@gmail.com>
2026-07-15 16:11:18 +0200

    refactor: remove font imports from CSS and move to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
+import '@fontsource/raleway/400.css';
+import '@fontsource/raleway/500.css';
+import '@fontsource/raleway/600.css';
+import '@fontsource/raleway/700.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
